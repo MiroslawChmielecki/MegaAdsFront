b1e93390e25e7833cf75348b69b59cb46f0c3195
Miroslaw Chmielecki <misiak19@op.pl>
2022-05-18 15:44:25 +0200

    Add and implements SearchContext

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import '../../utils/fix-map-icon';
+import {SearchContext} from "../../contexts/search.context";
 
+import '../../utils/fix-map-icon';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+
+    //useEffect czyli zrób efekt uboczny to wszystko co nie jest renderowaniem widoku
+    //kod w tym useEffect uruchamia sie za kazdym razem gdy zmienia sie search
+    useEffect(() => {
+        console.log('Make request to search for', search)
+    }, [search]);
 
     return (
         <div className="map">
+            <h1>Search for: {search}</h1>
             <MapContainer center={[50.0211459,22.6680963]} zoom={15}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
